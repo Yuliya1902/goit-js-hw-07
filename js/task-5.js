a5b38de4f6d1f70db61a9a3cdd142f1039b20c10
Yuliya1902 <116121971+Yuliya1902@users.noreply.github.com>
2024-01-16 00:11:17 +0100

    errore7hw

--- js/task-5.js
+++ js/task-5.js
@@ -1,15 +1,19 @@
 
-const btnChange = document.querySelector(".change-color")
-const color = document.querySelector(".color")
-btnChange.addEventListener('click', onBtnClick)
-function onBtnClick() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  color.textContent = getRandomHexColor();
+ document.addEventListener("DOMContentLoaded", function () {
+   function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215)
+       .toString(16)
+       .padStart(6, "0")}`;
+   }
 
+   const changeColorButton = document.querySelector(".change-color");
+   const widget = document.querySelector(".widget");
+   const colorSpan = document.querySelector(".color");
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
-}
+   changeColorButton.addEventListener("click", () => {
+     const randomColor = getRandomHexColor();
+     document.body.style.backgroundColor = randomColor;
+     widget.style.backgroundColor = randomColor;
+     colorSpan.textContent = randomColor;
+   });
+ });
